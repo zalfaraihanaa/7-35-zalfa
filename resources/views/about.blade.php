24e5edbe3f556e36d5a7be64024a34926e8544eb
zalfaraihanaa <zalfaaraihanaa@gmail.com>
2022-01-20 15:20:46 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Zalfa | Home</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h3> Gulf Kanawut</h3>
+    <p> Gupi</p>
+    <img src="images/gupi.jpeg" alt="princess" width= "200px">
+
+</body>
+</html>
