f2d096cb4cbc6d95053168d97bb9a6634c221d51
zalfaraihanaa <zalfaaraihanaa@gmail.com>
2022-01-27 10:13:44 +0700

    first commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Zalfa | Home</title>
-</head>
-<body>
-    <h1>Halaman About</h1>
-    <h3> Gulf Kanawut</h3>
-    <p> Gupi</p>
-    <img src="images/gupi.jpeg" alt="princess" width= "200px">
+ @extends('layouts.main')   
 
-</body>
-</html>
+ @section('container')
+    <h1>Halaman About</h1>
+    <h3>{{ $nama }}</h3>
+    <p>{{ $email }}</p>
+    <img src="images/{{ $gambar }}" alt="{{ $nama }}" width= "200px">
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about', [
+        "title" => "About",
+        "nama" => "Zalfa Raihana Fauziah",
+        "email" => "3103120238@student.smktelkom-pwt.sch.id",
+        "gambar" => "gupi.jpeg"
+    ]);
 });
 
 Route::get('/gallery', function () {
-    return view('gallery');
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
 });
