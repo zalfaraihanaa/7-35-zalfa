ed421a3603c66ad67fee2b7b38da464fce797434
zalfaraihanaa <zalfaaraihanaa@gmail.com>
2022-03-09 23:00:04 +0700

    modul 6, 7

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,51 @@
+<header class="header-desktop">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="header-wrap">
+                            <form class="form-header" action="" method="POST">
+                                
+                            </form>
+                            <div class="header-button">
+                               
+                                <div class="account-wrap">
+                                    <div class="account-item clearfix js-item-menu">
+                                        <div class="image">
+                                            <img src="{{asset('admin/images//icon/avatar-01.jpg')}}" alt="{{ auth::user()->name}}" />
+                                        </div>
+                                        <div class="content">
+                                            <a class="js-acc-btn" href="#">{{ auth::user()->name}}</a>
+                                        </div>
+                                        <div class="account-dropdown js-dropdown">
+                                            <div class="info clearfix">
+                                                <div class="image">
+                                                    <a href="#">
+                                                        <img src="{{asset('admin/images//icon/avatar-01.jpg')}}" alt="{{ auth::user()->name}}" />
+                                                    </a>
+                                                </div>
+                                                <div class="content">
+                                                    <h5 class="name">
+                                                        <a href="#">{{ auth::user()->name}}</a>
+                                                    </h5>
+                                                    <span class="email">{{ auth::user()->email}}</span>
+                                                </div>
+                                            </div>
+                                           
+                                            <div class="account-dropdown__footer">
+                                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">Zalfa</a>
+  <div class="container">
+    <a class="navbar-brand" href="/">Fafa</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -13,20 +13,20 @@
           <a class="nav-link {{ ( $title === 'About') ? 'active' : '' }}" href="/about">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ( $title === 'Gallery') ? 'active' : '' }}"" href="/gallery">Gallery</a>
+          <a class="nav-link {{ ( $title === 'Gallery') ? 'active' : '' }}" href="/gallery">Gallery</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ( $title === 'Contacts') ? 'active' : '' }}" href="/contacts">Contacts Us</a>
+          <a class="nav-link {{ ( $title === 'Contacts') ? 'active' : '' }}" href="{{ route('contacts.create')}}">Contact Us</a>
         </li>
       </ul>
       <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-              <a class="nav-link" href="{{route ('login')}}">Login</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="{{route ('register')}}">Register</a>
-          </li>
-        </ul
+        <li class="nav-item">
+          <a class="nav-link" href="{{route ('login')}}">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route ('register')}}">Register</a>
+        </li>
+      </ul>
     </div>
   </div>
 </nav>
